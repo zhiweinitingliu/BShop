cbae5ad7ab2991a3ce702d8fe27cfa470bc7fd5d
zhiweinitingliu <a15939582085@126.com>
2017-05-27 17:20:23 +0800

    Initial commit

---
+++ app/src/main/java/com/jybd/bshop/app/MyShopApplication.java
@@ -0,0 +1,13 @@
+package com.jybd.bshop.app;
+
+import android.app.Application;
+
+/**
+ * @Author : wdk
+ * @Email : a15939582085@126.com
+ * created on : 2017/5/27 14:24
+ * @Description :自定义application
+ */
+
+public class MyShopApplication extends Application {
+}
---
+++ app/src/main/java/com/jybd/bshop/ui/home/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jybd.bshop.ui.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jybd.bshop.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
