9f319d9b783526d586d733640cd758a8d826c9e9
zhiweinitingliu <a15939582085@126.com>
2017-05-31 09:50:49 +0800

    引入第三方框架

--- app/src/main/java/com/jybd/bshop/app/MyShopApplication.java
+++ app/src/main/java/com/jybd/bshop/app/MyShopApplication.java
@@ -2,6 +2,13 @@ package com.jybd.bshop.app;
 
 import android.app.Application;
 
+import com.jybd.bshop.BuildConfig;
+import com.yanzhenjie.nohttp.Logger;
+import com.yanzhenjie.nohttp.NoHttp;
+import com.yanzhenjie.nohttp.OkHttpNetworkExecutor;
+import com.yanzhenjie.nohttp.cache.DBCacheStore;
+import com.yanzhenjie.nohttp.cookie.DBCookieStore;
+
 /**
  * @Author : wdk
  * @Email : a15939582085@126.com
@@ -10,4 +17,21 @@ import android.app.Application;
  */
 
 public class MyShopApplication extends Application {
+    private static Application _instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        _instance = this;
+
+        Logger.setDebug(BuildConfig.DEBUG);// 开启NoHttp的调试模式, 配置后可看到请求过程、日志和错误信息。
+        Logger.setTag("NoHttpSample");// 设置NoHttp打印Log的tag。
+
+        // 一般情况下你只需要这样初始化：
+        NoHttp.initialize(this);
+    }
+
+    public static Application getInstance() {
+        return _instance;
+    }
 }
