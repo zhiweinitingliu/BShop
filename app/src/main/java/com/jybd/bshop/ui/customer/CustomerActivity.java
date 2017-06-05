2138156dc3d216d50562beeb924f2b4b98df0287
zhiweinitingliu <a15939582085@126.com>
2017-06-05 09:23:11 +0800

    创建客户activity、

---
+++ app/src/main/java/com/jybd/bshop/ui/customer/CustomerActivity.java
@@ -0,0 +1,24 @@
+package com.jybd.bshop.ui.customer;
+
+import android.os.Bundle;
+
+import com.jybd.bshop.base.BaseActivity;
+
+/**
+ * @Author : wdk
+ * @Email : a15939582085@126.com
+ * created on : 2017/6/5 9:22
+ * @Description :客户activity
+ */
+
+public class CustomerActivity extends BaseActivity {
+    @Override
+    public void onActivityCreate(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
